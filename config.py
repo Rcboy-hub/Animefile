506a547eb0354241bbd80bb831fa1feaa49c6059
Rcboy-hub <harishentreprenur@gmail.com>
2025-01-24 22:53:07 -0800

    Update config.py

--- config.py
+++ config.py
@@ -35,8 +35,7 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://harish:Anime%23%23%40%40@cluster0.5qxpw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-")
+DB_URI = environ.get("DB_URI", "mongodb+srv://harish:Anime%23%23%40%40@cluster0.5qxpw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "Cluster0")
 
 # Auto Delete Information
